d4f00bf281caec3b6a5505c4a7c218a0da866600
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-04-29 14:58:05 +0700

    fix login

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const jsreport = require('jsreport')()
 
+const PORT = process.env.PORT || 3000
+
 if (process.env.JSREPORT_CLI) {
   // export jsreport instance to make it possible to use jsreport-cli
   module.exports = jsreport
 } else {
   jsreport.init().then(() => {
-    // running
+    console.log(`jsReport server running on port ${PORT}`)
   }).catch((e) => {
     // error during startup
     console.error(e.stack)
