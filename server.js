2fd227bc20c21dec55403800fdac9186270e9422
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-04-29 19:58:30 +0700

    fix migration + postgres setup + seeder

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const jsreport = require('jsreport')()
 const PORT = process.env.PORT || 3000
 
 if (process.env.JSREPORT_CLI) {
-  // export jsreport instance to make it possible to use jsreport-cli
   module.exports = jsreport
 } else {
   jsreport.init().then(() => {
     console.log(`jsReport server running on port ${PORT}`)
   }).catch((e) => {
-    // error during startup
+    console.error('jsreport startup failed:')
     console.error(e.stack)
     process.exit(1)
   })
